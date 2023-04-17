dc689cd27ede7d69beb13ac0db0fa4630a7f50a4
alanjxs@gmail.com <alanjxs@gmail.com>
2023-04-17 11:15:56 -0300

    Criação do contador de caracteres e obtenção da menssagem!

--- src/app/components/new-post/new-post.component.ts
+++ src/app/components/new-post/new-post.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent {
+	tweetMessage: string = ""
+	tweetMaxLength: number = 240
+	tweetRemain: number = this.tweetMaxLength
+
+	sendTweet(){
+		alert(this.tweetMessage)
+	}
+
+	changeTweet(){
+		this.tweetRemain = 240 - this.tweetMessage.length
+	}
 
 }
